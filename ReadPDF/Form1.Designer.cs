1603b138bcea69d17a9abe4994e5b5fa7689bc96
rdevalco <roddeval@uat.edu>
2021-11-26 17:09:59 -0600

    Add project files.

---
+++ ReadPDF/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace ReadPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnProcessFile = new System.Windows.Forms.Button();
+            this.txtInformation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(51, 27);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(29, 13);
+            this.lblPath.TabIndex = 0;
+            this.lblPath.Text = "Path";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(54, 53);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(23, 13);
+            this.lblFile.TabIndex = 3;
+            this.lblFile.Text = "File";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(83, 23);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(346, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(83, 49);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(217, 20);
+            this.txtFile.TabIndex = 4;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(454, 27);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(75, 23);
+            this.btnPath.TabIndex = 2;
+            this.btnPath.Text = "Get Path";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(454, 57);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 5;
+            this.btnFile.Text = "Get File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnProcessFile
+            // 
+            this.btnProcessFile.Location = new System.Drawing.Point(454, 86);
+            this.btnProcessFile.Name = "btnProcessFile";
+            this.btnProcessFile.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessFile.TabIndex = 6;
+            this.btnProcessFile.Text = "Process File";
+            this.btnProcessFile.UseVisualStyleBackColor = true;
+            this.btnProcessFile.Click += new System.EventHandler(this.btnProcessFile_Click);
+            // 
+            // txtInformation
+            // 
+            this.txtInformation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInformation.Location = new System.Drawing.Point(26, 159);
+            this.txtInformation.Multiline = true;
+            this.txtInformation.Name = "txtInformation";
+            this.txtInformation.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInformation.Size = new System.Drawing.Size(516, 271);
+            this.txtInformation.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 450);
+            this.Controls.Add(this.txtInformation);
+            this.Controls.Add(this.btnProcessFile);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lblPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnProcessFile;
+        private System.Windows.Forms.TextBox txtInformation;
+    }
+}
+
---
+++ ReadPDF/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iText.Forms;
+using iText.Kernel.Pdf;
+using iText.Forms.Fields;
+using System.IO;
+
+namespace ReadPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPath.Text = Application.ExecutablePath;
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            //PdfAcroForm
+            string sPath = "";
+            string sSelectedPath = "";
+            sPath = txtPath.Text;
+            if (sPath.Length == 0)
+            {
+                sPath = Application.StartupPath;
+            }
+            if (sPath.EndsWith("\\") == false)
+            {
+                sPath += "\\";
+            }
+
+            folderBrowserDialog1.SelectedPath = sPath;
+            DialogResult result = folderBrowserDialog1.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                sSelectedPath = folderBrowserDialog1.SelectedPath;
+                if (sSelectedPath.EndsWith("\\") == false)
+                {
+                    sSelectedPath += "\\";
+                }
+                txtPath.Text = sSelectedPath;
+                this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            }
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            string sPath = "";
+            string sSelectedFile = "";
+            string sFile = "";
+            int nStartAt = 0;
+            sPath = txtPath.Text;
+            if (sPath.Length > 0)
+            {
+                this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+                if (sPath.EndsWith("\\") == false)
+                {
+                    sPath += "\\";
+                }
+                openFileDialog1.Filter = "PDF Files|*.pdf|Text Files|*.txt|All Files|*.*";
+                openFileDialog1.DefaultExt = "*.pdf";
+                openFileDialog1.InitialDirectory = sPath;
+                openFileDialog1.FileName = "";
+                DialogResult result = openFileDialog1.ShowDialog(this);
+                if (result == DialogResult.OK)
+                {
+                    sSelectedFile = openFileDialog1.FileName;
+                    nStartAt = sPath.Length;
+                    sFile = sSelectedFile.Substring(nStartAt);
+                    txtFile.Text = sFile;
+                }
+            }
+        }
+
+        private string GetFormFieldNames(PdfAcroForm pdfAcroForm)
+        {
+            return string.Join("\r\n", pdfAcroForm.GetFormFields()
+                                           .Select(x => x.Key).ToArray());
+        }
+
+        private string GetFormFieldNamesWithValues(PdfAcroForm pdfAcroForm)
+        {
+
+            return string.Join("\r\n", pdfAcroForm.GetFormFields()
+                                           .Select(x => x.Key + "=" +
+                                            pdfAcroForm.GetField(x.Key))
+                                           .ToArray());
+        }
+
+        private void ProcessForm(PdfAcroForm pdfAcroForm)
+        {
+            IDictionary<string, PdfFormField> fields = null;
+            PdfFormField item = null;
+            PdfString pdfString = null;
+            PdfName pdfName = null;
+            string value = "";
+            Type type = null;
+            string value2 = "";
+            if (pdfAcroForm != null)
+            {
+                fields = pdfAcroForm.GetFormFields();
+                if (fields != null)
+                {
+                    foreach (string key in fields.Keys)
+                    {
+                        fields.TryGetValue(key,out item);
+                        if (item != null)
+                        {
+                            Debug.WriteLine(string.Format("key: {0}\r\nValue: {1}\r\n",key,item.GetValueAsString()));
+
+                            pdfName= item.GetFormType();
+                            if (pdfName != null)
+                            {
+                                value = pdfName.GetValue();
+                                Debug.WriteLine(string.Format("item: pdfName.GetFormType: {0}\r\n", value));
+
+                                type = pdfName.GetType();
+                                Debug.WriteLine(string.Format("item: pdfName.GetType: {0}\r\n", type.ToString()));
+
+                                value2 = pdfName.GetValue();
+                                Debug.WriteLine(string.Format("item: pdfName.GetValue: {0}\r\n", value2));
+                                
+                                pdfName.GetObjectType();
+                            }
+
+                            pdfString = item.GetAlternativeName();
+                            if (pdfString != null)
+                            {
+                                value = pdfString.GetValue();
+                                Debug.WriteLine(string.Format("item: pdfString.GetAlternativeName: {0}\r\n", value));
+                            }
+
+                            
+
+                            Debug.WriteLine("----------\r\n");
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ProcessFile(string sPath, string sFile)
+        {
+
+            string sPathFile = "";
+            iText.Kernel.Pdf.PdfReader pdfReader = null;
+            iText.Kernel.Pdf.PdfDocument pdfDocument = null;
+            PdfAcroForm pdfAcroForm = null;
+            iText.Forms.Fields.PdfFormField pdfFormField = null;
+            PdfString pdfString = null;
+            PdfString pdfString2 = null;
+            IDictionary<string, iText.Forms.Fields.PdfFormField> fields = null;
+            string sValue = "";
+            string sValue2 = "";
+            string key = "";
+            string messageText = "";
+            StringBuilder stringBuilder = null;
+
+            sPathFile = sPath;
+            if (sPathFile.EndsWith("\\") == false)
+            {
+                sPathFile += "\\";
+            }
+            sPathFile += sFile;
+
+            if (File.Exists(sPathFile) == true)
+            {
+                stringBuilder = new StringBuilder();
+
+                pdfReader = new PdfReader(sPathFile);
+
+                pdfDocument = new PdfDocument(pdfReader);
+
+                pdfAcroForm = PdfAcroForm.GetAcroForm(pdfDocument, false);
+
+                if (pdfAcroForm != null)
+                {
+
+                    messageText = GetFormFieldNamesWithValues(pdfAcroForm);
+
+                    stringBuilder.Append(messageText);
+                    stringBuilder.Append("\r\n----------\r\n");
+                    messageText = stringBuilder.ToString();
+                    txtInformation.Text = messageText;
+
+                    ProcessForm(pdfAcroForm);
+
+                    //fields = pdfAcroForm.GetFormFields();
+
+                    //if (fields.Count > 0)
+                    //{
+
+                    //    foreach (KeyValuePair<string, iText.Forms.Fields.PdfFormField> keyValue in fields)
+                    //    {
+
+                    //        key = keyValue.Key;
+                    //        pdfFormField = keyValue.Value;
+
+                    //        if (pdfFormField != null)
+                    //        {
+                    //            //messageText = string.Format("{0}: {1}\r\n", key, pdfFormField.GetFieldName().ToString());
+                    //            //stringBuilder.Append(messageText);
+
+                    //            //pdfString = pdfFormField.GetFieldName();
+                    //            //sValue = (pdfString != null) ? pdfString.GetValue() : "";
+
+                    //            //pdfString2 = pdfFormField.GetMappingName();
+                    //            //sValue2 = (pdfString2 != null) ? pdfString2.GetValue() : "";
+
+                    //            //messageText = string.Format("Field Name {0}\r\nMapping Name: {1}\r\n----------\r\n", sValue, sValue2);
+
+                    //        }
+
+                    //    }
+                    //    messageText = stringBuilder.ToString();
+                    //    txtInformation.Text = messageText;
+
+                    //}
+                }
+
+            }
+
+
+        }
+
+        private void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            //PdfAcroForm
+            string sPath = "";
+            string sFile = "";
+            StringBuilder stringBuilder = null;
+            string messageText = "";
+            Exception exceptionDetails = null;
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                sPath = txtPath.Text;
+                sFile = txtFile.Text;
+
+                if ((string.IsNullOrEmpty(sPath) == false) &&
+                    (string.IsNullOrEmpty(sFile) == false))
+                {
+                    txtInformation.Text = "";
+                    ProcessFile(sPath, sFile);
+                }
+                else
+                {
+                    messageText = "Please provide a path/filename before pressing Process File!";
+                    MessageBox.Show(messageText, "oops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Debug.WriteLine(messageText);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                stringBuilder = new StringBuilder();
+                exceptionDetails = exception;
+
+                while (exceptionDetails != null)
+                {
+
+                    messageText = "\r\nMessage: " + exceptionDetails.Message + "\r\nSource: " + exceptionDetails.Source + "\r\nStack Trace: " + exceptionDetails.StackTrace + "\r\n----------\r\n";
+
+                    stringBuilder.Append(messageText);
+
+                    exceptionDetails = exceptionDetails.InnerException;
+
+                }
+
+                messageText = stringBuilder.ToString();
+                Debug.WriteLine(messageText);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+    }
+}
